52ea3e4431a35d3d31c535f0483eec07680ce6dc
Viktor Brusylovets <v.brusylovets@gmail.com>
2015-03-13 17:01:04 +0200

    fix tests and mongo db names for test and prod modified

--- src/main/java/app/WebSecurityConfig.java
+++ src/main/java/app/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
--- src/test/java/app/ApiUserControllerTest.java
+++ src/test/java/app/ApiUserControllerTest.java
@@ -4,12 +4,18 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,30 +23,31 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:8181")
 public class ApiUserControllerTest {
-//
-//    @Autowired
-//    private UserRepository repository;
 
     private MockMvc mvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new ApiUserController()).build();
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void registerUser() throws Exception {
-        core.User user = new core.User();
-        user.setUsername("ut1");
-        user.setPassword("ut1p");
         String json = new ObjectMapper().writeValueAsString(new Object() {
             private String username = "u1";
             private String password = "u1p";
@@ -61,7 +68,7 @@ public class ApiUserControllerTest {
                 this.username = username;
             }
         });
-        mvc.perform(put("/api/user").content(json).contentType(MediaType.APPLICATION_JSON))
+        mvc.perform(put("http://localhost:8181/api/user").content(json).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
 //                .andExpect(content().json("username"))
 //                .andExpect(content().json("password"))
